cc2f131d724a3011f3032d60076cf0b6c9ae26b7
Linda Nasredin <linda.nasredin@imperva.com>
2023-02-08 19:03:55 +0200

    Supported GW HADR (#83)
    * EDSF-68 Create GW HADR - before moving secret
    
    * EDSF-68 Create GW HADR - before changing GW HADR keys
    
    * EDSF-68 Create GW HADR - validate passed, before script
    
    * EDSF-68 Create GW HADR - script coded not tested
    
    * EDSF-68 Create GW HADR - applying
    
    * EDSF-68 Create GW HADR - applying 2
    
    * EDSF-68 Create GW HADR - fixed basic_deployment
    
    * EDSF-68 Create GW HADR - fixed key exchange in hadr_deployment
    
    * EDSF-68 Create GW HADR - uncommented federation and onboarding in hadr_deployment
    
    * EDSF-68 Create GW HADR - fixed comments
    
    * EDSF-68 Create GW HADR - added secondary gws to outputs
    
    * EDSF-68 Create GW HADR - ranamed agentless_gw_group to agentless_gw_group_primary + removed hub dependency from module agentless_gw_group_hadr (it's already included in federation dependency)
    
    * EDSF-68 Create GW HADR - all passed
    
    * EDSF-68 Create GW HADR - replaced proxy's 3 attributes with 1 proxy_info object & fixed description
    
    * Adjusted github Apply CLI action to changes in deployment outputs
    
    * Code review fixes
    
    * README fixes
    
    * Attempt to fix Apply Cli action
    
    * Reverted temporary code: Used modules from registry instead of local
    
    * Fixed Apply CLI - defined $GW_OUTPUT due to difference between basic and hadr deployment outputs

--- _modules/sonar-base-instance/iam_role.tf
+++ _modules/sonar-base-instance/iam_role.tf
@@ -1,9 +1,9 @@
 #################################
-# Hub IAM role
+# DSF node IAM role
 #################################
 
 locals {
-  role_arn  = var.role_arn != null ? var.role_arn : try(aws_iam_role.dsf_hub_role[0].arn, null)
+  role_arn  = var.role_arn != null ? var.role_arn : try(aws_iam_role.dsf_node_role[0].arn, null)
   role_name = split("/", local.role_arn)[1] //arn:aws:iam::xxxxxxxxx:role/role-name
   role_assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -52,15 +52,15 @@ locals {
   )
 }
 
-resource "aws_iam_instance_profile" "dsf_hub_instance_iam_profile" {
-  name_prefix = "dsf-hub-instance-iam-profile"
+resource "aws_iam_instance_profile" "dsf_node_instance_iam_profile" {
+  name_prefix = "dsf-${var.resource_type}-instance-iam-profile"
   role        = local.role_name
 }
 
-resource "aws_iam_role" "dsf_hub_role" {
+resource "aws_iam_role" "dsf_node_role" {
   count               = var.role_arn != null ? 0 : 1
-  name_prefix         = "imperva-dsf-hub-role"
-  description         = "imperva-dsf-hub-role-${var.friendly_name}"
+  name_prefix         = "imperva-dsf-${var.resource_type}-role"
+  description         = "imperva-dsf-${var.resource_type}-role-${var.name}"
   managed_policy_arns = null
   assume_role_policy  = local.role_assume_role_policy
   inline_policy {
